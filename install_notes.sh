2b88007462546eaec526795330a49ed83cf73a02
Branislav <bandza88@gmail.com>
2018-01-26 19:54:52 +0100

    Made simple board on center of screen, with kaya texture.

--- install_notes.sh
+++ install_notes.sh
@@ -2,7 +2,9 @@ sudo cp joseki.xml /usr/lib/livewallpaper/plugins/joseki/
 sudo cp joseki.py /usr/lib/livewallpaper/plugins/joseki/
 sudo cp joseki.plugin /usr/lib/livewallpaper/plugins/joseki/
 sudo cp net.launchpad.livewallpaper.plugins.joseki.gschema.xml /usr/share/glib-2.0/schemas/
+sudo cp kaya.png /usr/share/livewallpaper/plugins/joseki/
 sudo /usr/bin/glib-compile-schemas /usr/share/glib-2.0/schemas/
+pkill livewallpaper
 livewallpaper-config
 
 #make /usr/share/livewallpaper/plugins/joseki and insert sgf files
